7b8439b87e73411baf4d516ef2fae99429335ea7
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-03-14 18:21:26 +0000

    3

--- cadastro2.py
+++ cadastro2.py
@@ -1,4 +1,7 @@
 nome =str(input("digite seu nome? "))
 idade = int(input("qual sua idade? "))
 cpf = float(input("digite seu cpf? "))
-print(f"você confirma seu cadastro? \n nome: {nome} \n idade: {idade} \n cpf: {cpf} ")
+
+
+
+
--- jogoforca.py
+++ jogoforca.py
@@ -2,19 +2,22 @@ print("**************************")
 print("Bem vindo ao jogo da forca")
 print("**************************")
 
-palavra_secreta = "amora"
+palavra_secreta = "amora" .upper()
 
 letras_acertadas = ["_", "_", "_", "_", "_"]
 print(letras_acertadas)
 
 enforcou = False
 acertou = False
+erros = 0
 
 while(not enforcou and not acertou):
     chute = input("Qual a letra? ")
-    chute = chute.strip()
+    chute = chute.strip().upper()
 
-    index = 0
+    if letra in palavra_secreta):
+      
+        index = 0
 
     for letra in palavra_secreta:
         if (chute.upper() == letra.upper()):
