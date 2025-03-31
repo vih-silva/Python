946523f5f31d32660a5720ba14b9cb6dd6f9c634
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-03-31 17:59:14 +0000

    g

--- contaOO.py
+++ contaOO.py
@@ -5,3 +5,7 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+        def extrato(self):
+            print("Saldo de ${self.saldo} do titular ${}"
+            "
---
+++ parouimpar.py
@@ -0,0 +1,9 @@
+import math
+
+numero = int(input("Digite um número inteiro: "))
+
+if math.fmod(numero, 2) != 0:
+    print("É impar")
+else:
+    print("É par")
+       
