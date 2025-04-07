d1b8074702f0fbd467aa82596f12bf2e8a5881d5
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-04-07 18:17:32 +0000

    rd

--- contaOrientada.py
+++ contaOrientada.py
@@ -1,10 +1,10 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
         print(f"Saldo de ${self.saldo} do titular ${self.titular}")
