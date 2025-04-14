dcc508fab502b52d11aa01d3373fc18d96b1596d
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-04-14 19:46:37 +0000

    dg

--- streaming.py
+++ streaming.py
@@ -31,6 +31,26 @@ class Serie:
     @property
     def likes(self):
         return self.__likes
-    
+   
+class Serie:
+    def __init__(self, nome, ano, temporada):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporada = temporada
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
     def dar_likes(self):
         self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome   
+  
+    @nome.setter
+    def nome(self,nome):
+        self.__nome = nome
+        
