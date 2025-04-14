85da8737435c20ea24eda2be84e3840ee081d71a
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-04-14 19:11:27 +0000

    ev

--- streaming.py
+++ streaming.py
@@ -1,23 +1,21 @@
 class Filme:
-    def __init__(self, nome, ano, duracap, imdb):
+    def __init__(self, nome, ano, duracao):
         self.__nome = nome.title()
         self.ano = ano 
         self.duracao = duracao
         self.__likes = 0
 
     @property
-    deef likes(self):
-    return self.__likes
+    def likes(self):
+        return self.__likes
 
-def dar_likes(self):
-    self.__likes += 1
+    def dar_likes(self):
+        self.__likes += 1
 
     @property
     def nome(self):
-        return self.__nome
-    @property
-    def imdb(self):
-        return self.imdb
+        return self.__nome   
+  
     @nome.setter
     def nome(self,nome):
         self.__nome = nome
