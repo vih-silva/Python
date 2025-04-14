cf8e811b7727456f406fc1d75d0fe6991a3e36c2
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-04-14 19:29:13 +0000

    we

--- streaming.py
+++ streaming.py
@@ -19,3 +19,18 @@ class Filme:
     @nome.setter
     def nome(self,nome):
         self.__nome = nome
+
+class Serie:
+    def __init__(self, nome, ano, temporada, imdb):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporada = temporada
+        self.imdb = imdb
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+    
+    def dar_likes(self):
+        self.__likes += 1
